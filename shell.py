980c95dbc3e481434b19b95e8e54b764cb393804
Vishnu K <64530007+VishDroid-dev@users.noreply.github.com>
2021-08-23 11:43:38 -0400

    exit function added!

--- shell.py
+++ shell.py
@@ -5,10 +5,19 @@ import os
 class spear(Cmd):
     intro = "5P34R shell"
     prompt = '(5P34R) => '
-    
-    def default(self, arg):
-        print(os.system(arg))
+                           
 
+    def default(self, arg):
+        if arg in ['quit', 'q', 'close', 'exit']:
+            self.close()
+        else:
+            print(os.system(arg)) 
+        
+        
+    def close(self):    
+        print("Exiting...")
+        exit()
 
 shell = spear()
 shell.cmdloop()
+
